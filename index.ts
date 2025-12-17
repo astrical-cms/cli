faa4000886557863987b3948bee16532ae4f580c
Adrian Webb <adrian@webb.sh>
2025-12-17 18:04:30 -0500

    Updating the CLI core version and adding version parameter to CLI configuration.

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-import { CLI, logger, findProjectRoot, setDebugMode } from '@nexical/cli-core';
+import { CLI, findProjectRoot } from '@nexical/cli-core';
 import { fileURLToPath } from 'node:url';
 import { discoverCommandDirectories } from './src/utils/discovery.js';
+import pkg from './package.json';
 import path from 'node:path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const commandName = 'astrical';
 const projectRoot = await findProjectRoot(commandName, process.cwd()) || process.cwd();
-
 const additionalCommands = discoverCommandDirectories(projectRoot);
 
 const app = new CLI({
+    version: pkg.version,
     commandName: commandName,
     searchDirectories: [
         path.resolve(__dirname, './src/commands'),
